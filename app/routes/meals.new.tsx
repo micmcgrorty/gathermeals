39b9c5c6abcfc125c7d9c510669ba53209b64328
micmcgrorty <michaelmcgrorty2@gmail.com>
2023-03-29 15:19:34 +0100

    add a description to the new meal form

--- app/routes/meals.new.tsx
+++ app/routes/meals.new.tsx
@@ -48,63 +48,70 @@ export default function NewMealPage() {
   }, [actionData]);
 
   return (
-    <Form
-      method="post"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 8,
-        width: "100%",
-      }}
-    >
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Name: </span>
-          <input
-            ref={nameRef}
-            name="name"
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-            aria-invalid={actionData?.errors?.name ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.name ? "name-error" : undefined
-            }
-          />
-        </label>
-        {actionData?.errors?.name && (
-          <div className="pt-1 text-red-700" id="title-error">
-            {actionData.errors.name}
-          </div>
-        )}
-      </div>
+    <>
+      <p className="mb-2">
+        Use this form to add a new meal to your list of favourites. We'll need
+        the name of the recipe and also a link to the recipe. The link will be
+        used when generating your weekly shopping list.
+      </p>
+      <Form
+        method="post"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
+          width: "100%",
+        }}
+      >
+        <div>
+          <label className="flex w-full flex-col gap-1">
+            <span>Name: </span>
+            <input
+              ref={nameRef}
+              name="name"
+              className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+              aria-invalid={actionData?.errors?.name ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.name ? "name-error" : undefined
+              }
+            />
+          </label>
+          {actionData?.errors?.name && (
+            <div className="pt-1 text-red-700" id="title-error">
+              {actionData.errors.name}
+            </div>
+          )}
+        </div>
 
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Link to recipe: </span>
-          <input
-            ref={recipeRef}
-            name="recipe"
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-            aria-invalid={actionData?.errors?.recipe ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.recipe ? "recipe-error" : undefined
-            }
-          />
-        </label>
-        {actionData?.errors?.recipe && (
-          <div className="pt-1 text-red-700" id="title-error">
-            {actionData.errors.recipe}
-          </div>
-        )}
-      </div>
+        <div>
+          <label className="flex w-full flex-col gap-1">
+            <span>Link to recipe: </span>
+            <input
+              ref={recipeRef}
+              name="recipe"
+              className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+              aria-invalid={actionData?.errors?.recipe ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.recipe ? "recipe-error" : undefined
+              }
+            />
+          </label>
+          {actionData?.errors?.recipe && (
+            <div className="pt-1 text-red-700" id="title-error">
+              {actionData.errors.recipe}
+            </div>
+          )}
+        </div>
 
-      <div className="text-right">
-        <button
-          type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-        >
-          Save
-        </button>
-      </div>
-    </Form>
+        <div className="text-right">
+          <button
+            type="submit"
+            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          >
+            Save
+          </button>
+        </div>
+      </Form>
+    </>
   );
 }
